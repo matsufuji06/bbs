f9ca0b68d7a21c62c21b6cea4a8a63f8f0dac6e0
matsufuji06 <n.matsufuji06@gmail.com>
2022-04-12 21:01:56 +0900

    投稿詳細機能の実装

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <img src="member_picture/<?php echo h($picture); ?>" width="48" height="48" alt=""/>
             <?php endif; ?>
             <p><?php echo h($message); ?><span class="name">（by <?php echo h($name); ?>さん）</span></p>
-            <p class="day"><a href="view.php?id="><?php echo h($created); ?></a>
+            <p class="day"><a href="view.php?id=<?php echo h($id); ?>"><?php echo h($created); ?></a>
                 [<a href="delete.php?id=" style="color: #F33;">削除</a>]
             </p>
         </div>
